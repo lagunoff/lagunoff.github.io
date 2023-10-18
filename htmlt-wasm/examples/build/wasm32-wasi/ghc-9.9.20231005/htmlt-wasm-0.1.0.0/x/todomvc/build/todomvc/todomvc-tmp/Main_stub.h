10bdd779918257ad65468501983c1ad1a22476ea
Vladislav <vlad.lagunoff@gmail.com>
2023-10-18 20:30:46 +0400

    Add htmlt-wasm examples

---
+++ htmlt-wasm/examples/build/wasm32-wasi/ghc-9.9.20231005/htmlt-wasm-0.1.0.0/x/todomvc/build/todomvc/todomvc-tmp/Main_stub.h
@@ -0,0 +1,11 @@
+#include <HsFFI.h>
+#if defined(__cplusplus)
+extern "C" {
+#endif
+extern HsPtr app(HsPtr a1);
+extern HsPtr hs_malloc(HsInt a1);
+extern void hs_free(HsPtr a1);
+#if defined(__cplusplus)
+}
+#endif
+
